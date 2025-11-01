866c3d9192f6ea119223da7e37770e105aa1cb00
Ruben Broekx <broekxruben@gmail.com>
2025-11-01 17:05:28 +0100

    feat: add template health check endpoint (#7)
    * feat(template): add health check endpoint
    
    * chore: remove redundant hello world endpoint and introduce pydantic basemodels
    
    * fix: linting and testing
    
    * fix: incorrect endpoint for test.sh
    
    * fix: remove enum in basemodel

--- test.sh
+++ test.sh
@@ -7,13 +7,12 @@ rm -rf my-project
 with_fastapi_api=("false" "true")
 
 for with_fastapi_api in "${with_fastapi_api[@]}"; do
-    echo ""
-    echo "-----------------------------------------------------------"
-    echo "Testing Copier configuration with FastAPI API: ${with_fastapi_api} ..."
-    echo "-----------------------------------------------------------"
-    echo ""
+    echo  -e "\n-----------------------------------------------------------"
+    echo  -e "Testing Copier configuration with FastAPI API: ${with_fastapi_api} ..."
+    echo  -e "-----------------------------------------------------------\n"
 
     # Scaffold a Python project
+    echo  -e "\nScaffolding project..."
     uvx copier copy --vcs-ref=HEAD . my-project \
         --defaults \
         --data project_name="My Project" \
@@ -24,6 +23,7 @@ for with_fastapi_api in "${with_fastapi_api[@]}"; do
         --data python_version="3.12" \
         --data with_fastapi_api=${with_fastapi_api} \
 
+    echo  -e "\nInitializing Git..."
     cd my-project
     git config --global init.defaultBranch main
     git init
@@ -31,20 +31,26 @@ for with_fastapi_api in "${with_fastapi_api[@]}"; do
     git add .
 
     # Lint and test the project with a dev container
+    echo  -e "\nStarting Dev Container..."
     devcontainer up --remove-existing-container --workspace-folder .
+    echo  -e "\nLocking dependencies..."
     devcontainer exec --workspace-folder . uv lock
+    echo  -e "\nLinting project..."
     devcontainer exec --workspace-folder . poe lint
+    echo  -e "\nTesting project..."
     devcontainer exec --workspace-folder . poe test
 
     # Build and test the app service
+    echo  -e "\nBuilding app service..."
     docker compose build app
+    echo  -e "\nStarting app service..."
     docker compose up --force-recreate --detach app
     container_id=$(docker compose ps -q app)
     sleep 2
 
     # Verify API is accessible
     if [ "$with_fastapi_api" == "true" ]; then
-        # Verify container is still running
+        echo  -e "\nVerifying container is still running..."
         status=$(docker inspect --format='{{.State.Running}}' "$container_id")
         if [ "$status" != "true" ]; then
             docker compose logs app
@@ -52,14 +58,14 @@ for with_fastapi_api in "${with_fastapi_api[@]}"; do
             exit 1
         fi
 
-        # Check API is accessible
-        if ! curl -f 'http://localhost:8000/hello_world' > /dev/null; then
+        echo  -e "\nChecking API is accessible..."
+        if ! curl -f 'http://localhost:8000/health' > /dev/null; then
             docker compose logs app
             docker compose down
             exit 1
         fi
     else
-        # No API. Process may exit. --> Check container exit code instead (0 for graceful exit).
+        echo  -e "\nChecking if container ended gracefully..."
         exit_code=$(docker inspect --format='{{.State.ExitCode}}' "$container_id")
         if [ "$exit_code" != "0" ]; then
             docker compose logs app
@@ -68,12 +74,11 @@ for with_fastapi_api in "${with_fastapi_api[@]}"; do
         fi
     fi
 
+    echo  -e "\nRemoving project..."
     cd -
     rm -rf my-project
 done
 
-echo ""
-echo "-----------------------------------------------------------"
-echo "All tests passed!"
-echo "-----------------------------------------------------------"
-echo ""
+echo  -e "\n-----------------------------------------------------------"
+echo  -e "All tests passed!"
+echo  -e "-----------------------------------------------------------\n"
